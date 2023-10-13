5d4f3e6a1b6cd73633519e4f9f2d3c8d2dcd7015
Christian Holt <sidaroth@protonmail.com>
2023-10-13 19:05:28 +0200

    Disable linebreak-style in base-config

--- packages/base-eslint-config/rules/base-config.js
+++ packages/base-eslint-config/rules/base-config.js
@@ -18,6 +18,9 @@ module.exports = {
         ],
         'no-mixed-operators': ['error', { allowSamePrecedence: true, }],
 
+        // We do not care about linebreaks for linting to be consistent across OSes. Git will handle this for us with autocrlf and .gitattributes anyway.
+        'linebreak-style': 'off',
+
         // https://blog.javascripting.com/2015/09/07/fine-tuning-airbnbs-eslint-config/
         'max-len': [1, 160, 2, { ignoreComments: true }],
         'no-param-reassign': ['warn', { props: false }], // Due to object composition we need to be able to adjust props.
