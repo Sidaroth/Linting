2a5fd818c24b19dbcde1c210f3b38a67d9c0c0d9
Christian Holt <sidaroth@protonmail.com>
2024-11-22 09:46:52 +0100

    Remove airbnb

--- packages/vue-eslint-config/eslint.config.js
+++ packages/vue-eslint-config/eslint.config.js
@@ -2,6 +2,7 @@ import vueConfig from './rules/vue-config.js';
 import globals from 'globals';
 import oxlint from 'eslint-plugin-oxlint';
 import vueLint from "eslint-plugin-vue";
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 const config = [
     ...vueLint.configs['flat/recommended'],
@@ -10,6 +11,9 @@ const config = [
             ecmaVersion: 'latest',
             sourceType: 'module',
             globals: { ...globals.browser, ...globals.node },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
         },
         ignores: ['node_modules/**/*'],
     },
